e528f9a360589a2ea704eb81ae07466afeaee016
Korawit Wassanarungruengsuk <korawit.was@gmail.com>
2022-06-30 08:55:36 +0700

    edit service

--- API/repository/queue_db.go
+++ API/repository/queue_db.go
@@ -7,26 +7,8 @@ import (
 	"strings"
 	"time"
 
-	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
-
-func ConnectDatabase() (db *gorm.DB) {
-
-	//Set Data source name
-	dsn := "server=localhost\\SQLEXPRESS;Database=QueueSystem;praseTime=true"
-	dial := sqlserver.Open(dsn)
-
-	database, err := gorm.Open(dial, &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
-
-	if err != nil {
-		panic("Failed to connect to database!")
-	}
-	//auto migration
-	database.AutoMigrate(&model.QueueModel{})
-	return database
-}
 
 type queueRepositoryDB struct {
 	db *gorm.DB
