75f10dd2d4b5eaa9b8fa19f1b50f58b302eec750
Korawit Wassanarungruengsuk <korawit.was@gmail.com>
2022-07-05 13:50:22 +0700

    Edit handler

--- API/handler/queue.go
+++ API/handler/queue.go
@@ -1,78 +1,12 @@
 package handler
 
-import (
-	"net/http"
-	"q/model"
-	"q/service"
+import "github.com/gin-gonic/gin"
 
-	"github.com/gin-gonic/gin"
-)
-
-type queueHandler struct {
-	qService service.QueueService
-}
-
-func NewQueueHandler(qService service.QueueService) queueHandler {
-	return queueHandler{qService: qService}
-}
-
-func (h queueHandler) GetQueues(c *gin.Context) {
-	queues, err := h.qService.GetQueues()
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"data": queues})
-}
-
-func (h queueHandler) GetQueuesType(c *gin.Context) {
-	queues, err := h.qService.GetQueuesType(c.Param("Type"))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"data": queues})
-}
-
-func (h queueHandler) SearchQueue(c *gin.Context) {
-	name := c.Query("name")
-	types := c.Query("types")
-	queues, err := h.qService.SearchQueue(name, types)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"data": queues})
-}
-
-func (h queueHandler) GetQueue(c *gin.Context) {
-	queue, err := h.qService.GetQueue(c.Param("Code"))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"data": queue})
-}
-
-func (h queueHandler) AddQueue(c *gin.Context) {
-	var input model.QueueInput
-	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(http.StatusConflict, gin.H{"error": err.Error()})
-		return
-	}
-	queue, err := h.qService.AddQueue(input)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"data": queue, "message": "Created"})
-}
-
-func (h queueHandler) DeQueue(c *gin.Context) {
-	queue, err := h.qService.DeQueue(c.Param("Code"))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"data": queue, "message": "Deleted"})
+type QueueHandler interface {
+	GetQueues(c *gin.Context)
+	GetQueuesType(c *gin.Context)
+	SearchQueue(c *gin.Context)
+	GetQueue(c *gin.Context)
+	AddQueue(c *gin.Context)
+	DeQueue(c *gin.Context)
 }
