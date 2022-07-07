14c128c156bc4c3da07120c8e2786d833b5886d6
thanapat2402 <thanapat2402@gmail.com>
2022-07-07 08:27:50 +0700

    commit

--- Front-end/jest.config.js
+++ Front-end/jest.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   moduleNameMapper: {
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
+    "@core/(.*)": "<rootDir>/src/app/core/$1",
+    "@oshc(.*)": "<rootDir>/src/app/modules/oshc/$1",
+    "@shared(.*)": "<rootDir>/src/app/@shared/$1",
   },
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  moduleFileExtensions: ["json", "js", "jsx", "ts", "tsx", "vue", "cjs"],
+  moduleDirectories: ["node_modules", "src"],
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
 };
