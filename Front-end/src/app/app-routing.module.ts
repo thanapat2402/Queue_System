ddafa1311744dabd75430777aa78aebceb050573
thanapat2402 <thanapat2402@gmail.com>
2022-07-19 11:03:25 +0700

    update

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WebAdminComponent } from './components/web-admin/web-admin.component';
+import { WebCustomerComponent } from './components/web-customer/web-customer.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'admin', pathMatch: 'full' },
+  { path: '', redirectTo: 'customer', pathMatch: 'full' },
   { path: 'admin', component: WebAdminComponent },
+  { path: 'customer', component: WebCustomerComponent },
 ];
 
 @NgModule({
